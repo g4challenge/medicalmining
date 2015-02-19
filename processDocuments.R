ce857a1016e605c4f9f6a1bcef2b43f08e27c190
g4challenge <lukashuber1@gmail.com>
2015-02-19 10:11:55 +0100

    changed processing, with debug messages, still not working without warnings

--- processDocuments.R
+++ processDocuments.R
@@ -8,10 +8,29 @@ getDocument <- function(content){
   
   
   #### create Cleansed Content
-  dfcontent <- data.frame(matrix(unlist(clearcontent), nrow=3, byrow=T), stringsAsFactors = F)
+  if(is.null(clearcontent)) return("")
+  
+  if(is.null(clearcontent$date)){
+    clearcontent$date <- rep(0, length(clearcontent$text))
+  }
+  dfcontent <- tryCatch({
+      data.frame(matrix(unlist(clearcontent), nrow=3, byrow=T), stringsAsFactors = F)
+                 },
+                        error = function(e){
+                          print(e)
+                          #print(clearcontent)
+                         # stop("error")
+                        },
+                      warning = function(w){
+                        print(w)
+                        print(clearcontent)
+                        data.frame(matrix(unlist(clearcontent), nrow=3, byrow = T), stringsAsFactors = F)
+                      }
+      )
   #datum <- content$date[[2]]
   #date <- strptime(datum, "%d. %B %Y um %H:%M Uhr")
-  
+  #print("test")
+  #print(dfcontent)
   ### create a Document
   doc <- paste(gsub("[\t\n]", "", x=dfcontent[2,], useBytes = T), sep="", collapse = " NEXTENTRY ")
   return(c(contentTitle, doc))
@@ -22,12 +41,13 @@ getAllDocumentsofThematic <- function(df.threads){
   doc.list <- list()
   title.list <- list()
   ### Loop over Threads and get Content
-  for(j in 1:100){   #length(df.threads)){
+  for(j in 1:length(df.threads)){
     #j <- 4
     content <- scrapeContent(paste(url,"/" ,df.threads[2,j], sep=""))
     doc <- getDocument(content)
-    title.list <- list(title.list, list(doc[1]))
-    doc.list <- list(doc.list, list(doc[2]))
+    print(doc)
+    title.list <- c(title.list, doc[1])
+    doc.list <- c(doc.list, doc[2])
   }
   return(doc.list)
 }
--- runExtraction.R
+++ runExtraction.R
@@ -9,3 +9,8 @@ threads <- scrapeThematic(url, df[2,i])
 df.threads <- data.frame(matrix(unlist(threads), nrow=2, byrow=T), stringsAsFactors = F)
 
 docs <- getAllDocumentsofThematic(df.threads)
+
+docs <- getAllDocumentsofThematic(df.threads[, 1:100])
+
+url <- "board.netdoktor.de/beitrag/auf-einem-auge-heller-sehen-wie-kommt-das.249825/"
+scrapeContent(url)
