80e2759d43b1bd9ba8e6aa93165523c1cb0ff02e
g4challenge <lukashuber1@gmail.com>
2015-04-17 10:49:50 +0200

    implemented more or less complete tests and output to come

--- pachinkoClass.R
+++ pachinkoClass.R
@@ -95,14 +95,228 @@ pachinko <- R6Class(classname = "pachinkoModel",
                           
                           # Start the sampler
                           for(i in 1:numIterations){
+                            clearHistograms()
+                            sampleTopicsForAllDocs(random)
                             
+                            if(iterations > 0){
+                              #skipped 2 ifs
+                              if(optimizeInterval != 0 && (iterations %% outputModelInterval == 0)){
+                                for(superTopic in 1:self$numSuperTopics){
+                                  learnParameters(self$subAlphas[superTopic],
+                                                  self$subTopicHistograms[superTopic],
+                                                  self$superTopicHistograms[superTopic])
+                                  self$subAlphaSums[superTopic]=0.0
+                                  for(subTopic in 1:self$numSubTopics){
+                                    self$subAlphaSums[superTopic] <- self$subAlphaSums[superTopic] + self$subAlphas[superTopic, subTopic]
+                                  }
+                                }
+                              }
+                            }
+                          }
+                        }
+                        # end of constructor
+                      },
+                      clearHistograms = function(){
+                        for(superTopic in 1:numSuperTopics){
+                          self$superTopicHistograms[superTopic] = rep(0, length(self$superTopicHistograms[superTopic]))
+                          for(subTopic in 1:numSubTopics){
+                            self$subTopicHistograms[superTopic, subTopic] = rep(0, length(self$superTopicHistograms[superTopic, subTopic]))
+                          }
+                        }
+                      },
+                      learnParameters = function(parameters, observations, observationLengths){
+                        parameterSum = sum(parameters)
+                        
+                        noZeroLimits= rep(-1, length(observations))
+                        
+                        #   The histogram arrays go up to the size of the largest document,
+                        #		but the non-zero values will almost always cluster in the low end.
+                        #		We avoid looping over empty arrays by saving the index of the largest
+                        #		non-zero value.
+
+                        for(i in 1:dim(observations)[1]){
+                          for(k in 1:dim(observations)[2]){
+                            if(observation[i,k] > 0){
+                              noZeroLimits[i] = k
+                            }
+                          }
+                        }
+                      
+                       for(i in 1:200){
+                         #Calculate denominator
+                         denominator = 0.0
+                         currentDigamma = 0.0
+                         #Iterate over the histogram
+                         for(i in 1:length(observationLengths)){
+                           currentDigamma = currentDigamma + 1/(parameterSum + i - 1)
+                           denominator = denominator + observationLengths[i] * currentDigamma
+                         }
+                       }
+                       # Calculate the individual parameters
+                       parameterSum = 0
+                       
+                       for(k in 1:length(parameters)){
+                         # What's the largest non-zero element in the histogram
+                         nonZeroLimit = noZeroLimits[k]
+                         
+                         # If there are no tokens assigned to this super-sub pair
+                         # anywhere in the corpus bail
+                         
+                         if(nonZeroLimit == -1){
+                           parameters[k] = 0.000001
+                           parameterSum = parameterSum + 0.000001
+                           next
+                         }
+                         oldParametersK = parameters[k]
+                         parameters[k] = 0;
+                         currentDigamma = 0;
+                         
+                         histogram = observations[k]
+                         for(i in 1:nonZeroLimit){
+                           currentDigamma = currentDigamma + 1/(oldParametersK + i - 1)
+                           parameters[k] = parameters[k] + histogram[i] * currentDigamma
+                         }
+                         
+                         parameters[k] = parameters[k] * oldParametersK / denominator
+                         parameterSum = parameterSum + parameters[k]
+                       }
+                      },
+                      # One iteration of Gibbs sampling, across all documents.
+                      sampleTopicsForAllDocs = function(random){
+                        #Loop over every word in the corpus
+                        for(di in 1:length(self$superTopics)){
+                          sampleTopicsForOneDoc(ilist,
+                                                superTopics[di],
+                                                subTopics[di],
+                                                random)
+                        }
+                      },
+                      sampleTopicsForOneDoc = function(oneDocTokens,
+                                                       superTopics,
+                                                       subTopics,
+                                                       random){
+                        for(t in 1:numSuperTopics){
+                          self$superSubCounts[t] = rep(0, length(self$superSubCounts))
+                        }
+                        self$superCounts = rep(0, self$superCounts)
+                        
+                        #populate topic couns
+                        for(si in 1:length(oneDocTokens)){
+                          self$superSubCounts[superTopics[si], subTopics[si]] = self$superSubCounts[superTopics[si], subTopics[si]] + 1
+                          self$superCounts[superTopics[si]] = self$superCounts[superTopics[si]] + 1
+                        }
+                        # iterate over the positions(words) in the document
+                        for(si in 1:length(oneDocTokens)){
+                          type = getIndex(oneDocTokens[si])
+                          superTopic = superTopics[si]
+                          subTopic = subTopics[si]
+                          
+                          # Remove this token from all counts
+                          self$superSubCounts[superTopic, subTopic] = self$superSubCounts[superTopic, subTopic] - 1
+                          self$superCounts[superTopic] = self$superCounts[superTopic] -1
+                          self$typeSubTopicCounts[type, subTopics] = self$typeSubTopicCounts[type, subTopics] - 1
+                          self$tokensPerSuperTopic[superTopic] = self$tokensPerSuperTopic[superTopic] -1
+                          self$tokensPerSubTopic[subTopic] = self$tokensPerSubTopic[subTopic] -1
+                          self$tokensPerSuperSubTopic[superTopic, subTopic] = self$tokensPerSuperSubTopic[superTopic, subTopic] - 1
+                          
+                          # Build a distribution over super-sub topic pairs
+                          # for this token
+                          
+                          # Clear the data structures
+                          for(t in 1:self$numSuperTopics){
+                            self$superSubWeights[t] = rep(0.0, length(self$superSubWeights[t]))
+                          }
+                          self$superWeights = rep(0.0, length(self$superWeights))
+                          self$subWeights = rep(0.0, length(self$subWeights))
+                          self$cumulativeSuperWeights = rep(0.0, self$cumulativeSuperWeights)
+                          
+                          # The conditional probability of each super-sub pair is proportional
+                          #  to an expression with three parts, one that depends only on the 
+                          #  super-topic, one that depends only on the sub-topic and the word type,
+                          #  and one that depends on the super-sub pair.
+                          
+                          # Calculate each of the super-only factors first
+                          for(superTopic in 1:numSuperTopics){
+                            self$superWeights[superTopic] = (self$superCounts[superTopic] + self$alpha[superTopic]) /
+                                                            (self$superCounts[superTopic] + self$subAlphaSums[superTopic])
+                          }
+                          # Next Calculate the sub-only factors
+                          
+                          for(subTopic in 1:numSubTopics){
+                            self$subWeights[subTopic] = (self$typeSubTopicCounts[type, subTopic] + self$beta) /
+                                                        (self$tokensPerSubTopic[subTopic] + self$vBeta)
                           }
                           
+                          # Finally, put them together
+                          cumulativeWeight=0.0
+                          for(superTopic in 1:numSuperTopics){
+                            currentSuperSubWeights = self$superSupWeights[superTopic]
+                            currentSuperSubCounts = self$superSubCounts[superTopic]
+                            currentSubAlpha = self$subAlphas[superTopic]
+                            currentSuperWeigth = self$superWeights[superTopic]
+                            
+                            for(subTopic in 1:numSubTopics){
+                              currentSuperSubWeights[subTopic] = 
+                                  currentSuperWeight *
+                                  self$subWeights[subTopic] *
+                                  (currentSuperSubCounts[subtopic] + currentSubAlpha[subTopic])
+                              cumulativeWeight = cumulativeWeight + currentSuperSubWeights[subTopic]
+                            }
+                            self$cumulativeSuperWeights[superTopic] = cumulativeWeight
+                          }
+                          
+                          #Sample a topic assignment from this distribution
+                          sample = sample(random) *cumulativeWeight #TODO fixx
+                          
+                          # Go over the row sums to find the super-topic...
+                          superTopic = 0
+                          while(sample > self$cumulativeSuperWeights[superTopic]){
+                            superTopic = superTopic + 1
+                          }
+                          # Now read across to find the sub-topic
+                          currentSuperSubWeights = self$superSubWeights[superTopic]
+                          cumulativeWeight = self$cumulativeSuperWeights[superTopic] -
+                            currentSuperSubWeights[0];
+                          
+                          # Go over each sub-topic until the weight is LESS than
+                          #  the sample. Note that we're subtracting weights
+			                    #  in the same order we added them...
+			                    subTopic = 0;
+                          while(sample < cumulativeWeight){
+                            subTopic = subTopic + 1
+                            cumulativeWeight = cumulativeWeight - currentSuperSubWeights[subTopic]
+                          }
+                          
+                          # Save the coice into the Gibbs state
+                          
+                          superTopics[si] = superTopic
+                          subTopic[si] = subTopic
+                          
+                          # put the new super/sub topics into the counts
+			                    self$superSubCounts[superTopic, subTopic] = self$superSubCounts[superTopic, subTopic]+ 1
+			                    self$superCounts[superTopic] = self$superCounts[superTopic]+1
+			                    self$typeSubTopicCounts[type][subTopic] = self$typeSubTopicCounts[type][subTopic]+1
+			                    self$tokensPerSuperTopic[superTopic] = self$tokensPerSuperTopic[superTopic]+1
+			                    self$tokensPerSubTopic[subTopic] = self$tokensPerSubTopic[subTopic]+1
+			                    self$tokensPerSuperSubTopic[superTopic, subTopic] = self$tokensPerSuperSubTopic[superTopic, subTopic]+1
+                        }
+                        # Update the topic count histograms
+                        # for dirichlet estimation
+                        
+                        for(superTopic in 1:numSuperTopics){
+                          self$superTopicHistograms[superTopic, self$superCounts[superTopic]] = self$superTopicHistograms[superTopic, self$superCounts[superTopic]] +1
+                          currentSuperSubCounts = self$superSubCounts[superTopic,]
+                          
+                          for(subTopic in 1:numSupTopics){
+                            self$subTopicHistograms[superTopic, subTopic, currentSuperSubCounts[subTopic]] =
+                              self$subTopicHistograms[superTopic, subTopic, currentSuperSubCounts[subTopic]] +1
+                          }
                         }
                         
-                      },
-                      
-                      sampleTopicsForAllDocs <- function(){
                         
+                      
+                      
+                      
+                      
                       }
                       ))
