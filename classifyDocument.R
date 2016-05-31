b479cfca34540e53b0d61f77dc1d62c9cd1b03b1
g4challenge <lukashuber1@gmail.com>
2016-05-31 13:16:08 +0200

    removed unnecessary string

--- classifyDocument.R
+++ classifyDocument.R
@@ -14,8 +14,3 @@ classifyDoc <- function(doc, phi){
   }
   return(data.frame(Topics,commonTokens))
 }
-
-
-#doc <- "Bei der U8 wurde diese Untersuchung vorgenommen und es wurde uns mitgeteilt, dass wir einen Augenarzt mit dem Kind aufsuchen sollen. Beim Sehtest hat er alles gesehen, aber diese Untersuchung hat eine Sehschwäche ergeben. Braucht er nun eine Brille oder wie geht es weiter? Was bedeutet \"Auge abkleben\" und Sehschule, wie muss man sich das vorstellen?
-#Mit diesem Verfahren kann man erkennen inwieweit die Augen zusammen spielen, bzw. ob ein Auge weitsichtig ist und das andere beispielsweise kurzsichtig. Abkleben ist, wenn ein Auge sehr dominant ist und das andere dann sozusagen \"nichts\" mehr machen muss. Wenn das starke Auge abgeklebt wird, lernt das andere auch wieder was zu tun.
-#"
